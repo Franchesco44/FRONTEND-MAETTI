ee52b4d0bc5946e061ffe2bed5457a2a82f60de4
nasanchez7 <nadirblanco02@gmail.com>
2023-02-27 14:18:51 -0300

    cambiando estilo

--- components/Filtros/Filtros.js
+++ components/Filtros/Filtros.js
@@ -1,62 +1,56 @@
 import styles from "./Filtros.module.css"
 import { useSelector, useDispatch } from "react-redux"
-import { useEffect, useState } from "react"
-import { setFilterUbicacion, setFilterAlquiler, setFilterAll } from "../../redux/propiedadesSlice/propiedadesSlice"
+import { setFilter } from "../../redux/propiedadesSlice/propiedadesSlice"
+import { Dropdown } from "@nextui-org/react";
 
 const Filtros = () => {
     const dispatch = useDispatch()
     const propiedades = useSelector((state)=> state.propiedades.data)
     const isTranslate = useSelector((state) => state.translate.value)
 
-    const handleSelectUbicacion = (e) => {
-        dispatch(setFilterUbicacion(e.target.value))
-    }
-
-    const handleSelectAlquiler = (e) => {
-        dispatch(setFilterAlquiler(e.target.value))
-    }
-
-    const handleSelectAll = () => {
-        dispatch(setFilterAll())
-    }
-
     return(
         <div className={styles.filtrosContainer} >
             <h3>{isTranslate ? "Filter by":"Filtrar por"} </h3>
-            <div>
-                <div className={styles.listaFiltros}>
-                    <div className={styles.filtro} >
-                        <input onChange={handleSelectAll} name="todo" type="checkbox" value="todo"/>
-                        <label htmlFor="todo">{isTranslate ? "See all " : "Ver todo"}</label>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <strong> {isTranslate ? "Location" :"Ubicacion"} </strong>
-                <div className={styles.listaFiltros}>
+            <Dropdown>
+                <Dropdown.Button color={"default"} light>
+                    {isTranslate ? "Type of rental" :"Tipo de alquiler"}
+                </Dropdown.Button>
+                <Dropdown.Menu
+                    color={"default"}
+                    variant="light"
+                    aria-label="Actions"
+                    onAction={(key)=> dispatch(setFilter(key))}
+                >
+                    <Dropdown.Item 
+                        key="todo"
+                        >{isTranslate ? "See all" : "Ver todo"}</Dropdown.Item>
+                    <Dropdown.Item 
+                        key="noche"
+                        >{isTranslate ? "Rent per night" : "Alquiler por noche"}</Dropdown.Item>
+                    <Dropdown.Item
+                        key="mensual"
+                        >{isTranslate ? "Monthly rent" : "Alquiler mensual"}</Dropdown.Item>
+                    </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown>
+                <Dropdown.Button color={"default"} light>
+                    {isTranslate ? "Location" :"Ubicacion"}
+                </Dropdown.Button>
+                <Dropdown.Menu
+                    color={"default"}
+                    variant="light"
+                    aria-label="Actions"
+                    onAction={(key)=> dispatch(setFilter(key))}
+                >
                     {propiedades.map((p, index)=>{
                         return(
-                            <div key={index} className={styles.filtro} >
-                                <input onChange={handleSelectUbicacion} name="ubicacion" type="checkbox" value={p.ubicacion}/>
-                                <label htmlFor="ubicacion">{p.ubicacion}</label>
-                            </div>
+                            <Dropdown.Item
+                            key={p.ubicacion}
+                            >{p.ubicacion}</Dropdown.Item>
                         )
                     })}
-                </div>
-            </div>
-            <div>
-                <strong> {isTranslate ? "Type of rental" :"Tipo de alquiler"} </strong>
-                <div className={styles.listaFiltros}>
-                    <div className={styles.filtro}>
-                        <input onChange={handleSelectAlquiler} name="alquilernoche" type="checkbox" value="noche"/>
-                        <label htmlFor="alquilernoche">{isTranslate ? "Rent per night" : "Alquiler por noche"}</label>
-                    </div>
-                    <div className={styles.filtro}>
-                        <input onChange={handleSelectAlquiler} name="alquilermensual" type="checkbox" value="mensual"/>
-                        <label htmlFor="alquilermensual"> {isTranslate ? "Monthly rent" : "Alquiler mensual"} </label>
-                    </div>
-                </div>
-            </div>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     )
 }
--- redux/propiedadesSlice/propiedadesSlice.js
+++ redux/propiedadesSlice/propiedadesSlice.js
@@ -12,18 +12,18 @@ export const propiedadesSlice = createSlice({
       state.data = payload.payload
       state.dataCopy = payload.payload
     },
-    setFilterUbicacion: (state, payload) => {
-      state.dataCopy = state.data.filter((p) => p.ubicacion === payload.payload)
-    },
-    setFilterAlquiler: (state, payload) => {
-      state.dataCopy = state.data.filter((p) => p.alquiler === payload.payload)
-    },
-    setFilterAll: (state) => {
-      state.dataCopy = state.data
+    setFilter: (state, payload) => {
+      if(payload.payload === "todo"){
+        state.dataCopy = state.data
+      }else if(payload.payload === "noche" || payload.payload === "mensual"){
+        state.dataCopy = state.data.filter((p) => p.alquiler === payload.payload)
+      }else{
+        state.dataCopy = state.data.filter((p) => p.ubicacion === payload.payload)
+      }
     }
   }
 })
 
-export const { setPropiedades, setFilterUbicacion, setFilterAlquiler, setFilterAll } = propiedadesSlice.actions
+export const { setPropiedades, setFilterUbicacion, setFilterAlquiler, setFilterAll, setFilter } = propiedadesSlice.actions
 
 export default propiedadesSlice.reducer
